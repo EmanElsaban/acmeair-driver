a17f0403f84d2720c812fa0eb09a3855f27452d8
Eman Elsabban <eman.elsaban1@gmail.com>
2021-03-15 08:16:30 -0700

    Added dockerized jmeter
    added dockerfile and applyload.sh. Modified the hosts.csv to connect
    jmeter to the route
    
    Signed-off-by: Eman Elsabban <eman.elsaban1@gmail.com>

---
+++ acmeair-jmeter/scripts/applyLoad.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+cd $JMETER_HOME
+
+if [ $# -gt 0 ]
+  then
+    sed -i 's/localhost/'$1'/g' hosts.csv
+fi
+
+echo jmeter -n -t AcmeAir-microservices.jmx -DusePureIDs=true -j /output/acmeair.stats.0 -JHOST=$JHOST -JPORT=$JPORT -JTHREAD=$JTHREAD -JUSER=$JUSER -JURL=$JURL -JDURATION=$JDURATION -JRAMP=0 -JDELAY=0
+
+exec jmeter -n -t AcmeAir-microservices.jmx -DusePureIDs=true -j /output/acmeair.stats.0 -JHOST=$JHOST -JPORT=$JPORT -JTHREAD=$JTHREAD -JUSER=$JUSER -JURL=$JURL -JDURATION=$JDURATION -JRAMP=0 -JDELAY=0
